acc29367027b34d5cb4d12b590407c067529a9b4
law <law@alltrails.com>
2022-02-11 17:20:36 -0700

    add comments to local-deploy; fixup static HTML to be less horrifying

--- local-deploy.sh
+++ local-deploy.sh
@@ -2,12 +2,13 @@
 set -eo pipefail
 
 # You can pick a unique single-word namespace by passing it as an argument
-# to this script, or it'll try to make one for you from your 
+# to this script, or it'll try to make one for you from your local
 # machine's username
 
 AWS_ACCOUNT="310228935478"
 
-# This confirms we're pointing at the appropriate AWS account
+# This confirms we're pointing at the appropriate AWS account before trying
+# to do anything
 CURRENT_AWS_TARGET=$(aws sts get-caller-identity \
     | grep Account \
     | awk -F: '{print $2}' \
@@ -32,10 +33,14 @@ fi
 export COMMIT_ID=$(git rev-parse --verify --short HEAD)
 echo commit ID is $COMMIT_ID
 
+# This updates your local ~/.kube/config file with authentication info
+# for our test EKS cluster
 aws eks update-kubeconfig \
     --region us-west-2 \
     --name at-interviews-cluster
 
+# Then we log in to the Elastic Container Registry (ECR) so we have an 
+# AWS-accessible place to push the Docker container we're about to build...
 aws ecr get-login-password \
     --region us-west-2 \
     | docker login \
@@ -43,6 +48,8 @@ aws ecr get-login-password \
     --password-stdin \
     $AWS_ACCOUNT.dkr.ecr.us-west-2.amazonaws.com
 
+# Container gets built at this step.  Those tags are needed so the following
+# 'docker push' step sends the container to the right ECR repo
 docker build \
     --no-cache \
     --build-arg GIT_COMMIT=$COMMIT_ID \
@@ -50,10 +57,16 @@ docker build \
     -t $AWS_ACCOUNT.dkr.ecr.us-west-2.amazonaws.com/helloworld:$COMMIT_ID \
     .
 
+# If we've tagged our container appropriately above, this should send the 
+# container to ECR, where Kubernetes/Helm can pull it down
 docker push \
     $AWS_ACCOUNT.dkr.ecr.us-west-2.amazonaws.com/helloworld:$COMMIT_ID
 
-
+# This connects to Kubernetes (EKS) and tells it to deploy the above container
+# It also has a bunch of niceties in there around setting up an ALB (so we can
+# view it Across The Internet™, etc - this SHOULD be fairly hands-off.  Whatever
+# $COMMIT_ID this repo has, is sent to ECR as a tag, and EKS/k8s uses that to
+# pull down the appropriate build.  
 helm upgrade \
     --install \
     --namespace $NAMESPACE \
